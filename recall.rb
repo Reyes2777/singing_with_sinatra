77fdcc5109d38932164e75d762e674d41060b1d7
Reyes2777 <42840774+Reyes2777@users.noreply.github.com>
2018-12-19 14:12:47 -0500

    include datamaper in recall

--- recall.rb
+++ recall.rb
@@ -1,1 +1,16 @@
+require "rubygems"
+require "sinatra"
+require "datamaper"
 
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+ 
+class Note
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :complete, Boolean, :required => true, :default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+ 
+DataMapper.finalize.auto_upgrade!
